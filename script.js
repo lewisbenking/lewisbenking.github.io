a01fcb6078994f0df17a6ce40774e931cb10d995
Lewis King <lewisbk95@gmail.com>
2019-08-08 21:47:32 +0100

    Modal

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var x = document.getElementById('changingText'),
     phrases = ['Software Developer',  'First Class Graduate', 'Aston University Alumni', 'Musician'],
     i = 1;  
 
-setInterval(function (){
+    setInterval(function (){
  	// Iterate over the array
-  	x.innerHTML = phrases[i++];
+ 	x.innerHTML = phrases[i++];
   	// Reset i if it's at the end of the array
   	i = phrases.length === i ? 0 : i;
-}, 3500);
+  }, 3500);
